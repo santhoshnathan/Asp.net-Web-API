d5748cc9bbea6d4a9071ffcef5fd86f622432773
santhoshnathan <santhoshnathan04@gmail.com>
2015-08-22 20:33:00 +0530

    Complete Sample WebAPI

--- SampleWebAPI_Solution/SampleWebAPI/Controllers/ProductsController.cs
+++ SampleWebAPI_Solution/SampleWebAPI/Controllers/ProductsController.cs
@@ -1,12 +1,13 @@
-﻿using System.Web.Http;
-using SampleWebAPI.Models;
+﻿using SampleWebAPI.Models;
 using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
 
 namespace SampleWebAPI.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] _products = new Product[] 
+        readonly Product[] _products =
         { 
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
@@ -20,7 +21,7 @@ namespace SampleWebAPI.Controllers
 
         public IHttpActionResult GetProduct(int id)
         {
-            var product = _products.FirstOrDefault((p) => p.Id == id);
+            var product = _products.FirstOrDefault(p => p.Id == id);
             if (product == null)
             {
                 return NotFound();
