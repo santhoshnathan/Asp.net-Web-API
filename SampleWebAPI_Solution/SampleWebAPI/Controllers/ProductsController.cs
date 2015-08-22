9282f493e4cde2aeffcb651808e0cb0f01797e6e
santhoshnathan <santhoshnathan04@gmail.com>
2015-08-22 14:41:02 +0530

    Initial Commit

---
+++ SampleWebAPI_Solution/SampleWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using SampleWebAPI.Models;
+using System.Collections.Generic;
+
+namespace SampleWebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] _products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = _products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
